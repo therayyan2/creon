fff07c93b36ed6318f9743c2e6767715a70e5f52
therayyan2 <therayyanahmd@gmail.com>
2025-04-26 12:09:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,3 @@ st.set_page_config("Creon",page_icon="play4.png")
 st.title("_Hello_")
 st.subheader("I am Creon your Image and Video Generator-AI")
 st.write("🔥Turn Your Prompts Into Stunning Images & Videos!🔥")
-
-st.image("play4.png")
